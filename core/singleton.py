fd5b7f2351fd34382acb5430dd91eb843ce4f402
Clayton Powell <daustoe@gmail.com>
2013-10-28 22:55:28 -0700

    Adding singleton abstract class

---
+++ core/singleton.py
@@ -0,0 +1,14 @@
+'''
+Singleton class. Lets any class that inherits from this follow the singleton
+pattern. Only one of that class can be created.
+'''
+
+
+class Singleton(object):
+    _instances = {}
+
+    def __new__(class_, *args, **kwargs):
+        if class_ not in class_._instances:
+            class_._instances[class_] = super(Singleton, class_).__new__(class_,
+                *args, **kwargs)
+        return class_._instances[class_]
